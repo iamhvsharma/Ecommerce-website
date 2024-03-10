bb4ab1ea5363438bd0f19d580700ddb5fa946d02
iamhvsharma <itshvsharma@gmail.com>
2024-03-11 03:08:55 +0530

    Shop Context complete

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
 import Footer from '../src/Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+
+
 
 function App() {
   return (
@@ -16,9 +21,9 @@ function App() {
       <Navbar/>
       <Routes>
       <Route path='/' element={<Shop/>}/>
-      <Route path='/mens' element={<ShopCategory category="mens"/>}/>
-      <Route path='/womens' element={<ShopCategory category="women"/>}/>
-      <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+      <Route path='/mens' element={<ShopCategory banner = {men_banner} category="mens"/>}/>
+      <Route path='/womens' element={<ShopCategory banner = {women_banner} category="women"/>}/>
+      <Route path='/kids' element={<ShopCategory banner = {kid_banner} category="kid"/>}/>
       <Route path='product' element={<Product/>}/>
       <Route path=':productId' element={<Product/>}/>
       <Route path='/cart' element={<Cart/>}/>
